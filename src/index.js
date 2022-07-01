23f5d64972b6cca3cc606444f3e8be6e364910f5
aaronlrv <aaronvitharanage@gmail.com>
2022-07-01 21:34:07 +0400

    Fix function logic to allow more projects

--- src/index.js
+++ src/index.js
@@ -12,24 +12,26 @@ let projects= {
     
 }
 
-let projectname2 = "Asdfg"
 
-if (projects[projectsName] === projectname2) {
-    projects[projectsName] =  projects[projectsName].push(task)
+let projectsname2 = window.prompt("Whats your project name")
+let taskName2 = window.prompt("Whats your task!")
+let taskDate2 = window.prompt("When is this task due?")
+let taskPriority2 = window.prompt("How important is this task?")
+
+let task2 = new todolist (taskName2, taskDate2, taskPriority2)
+
+
+if (projects[projectsName] === projectsname2) {
+    projects[projectsName] =  projects[projectsName].push(task2)
   } 
 
-    if (!projectname2 in projects) {
+    if  (!(projectsname2 in projects)) {
         projects[projectsname2] = []
   }
 
   projects[projectsname2].push(task2)
   
-
-
-  
-
-
-
+  console.log(projects[projectsName])
 
 console.log(projects)
 
