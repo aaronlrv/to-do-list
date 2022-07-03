ba48e001b95e1008a701a8573b7fbfb52576efad
aaronlrv <aaronvitharanage@gmail.com>
2022-07-03 21:18:28 +0400

    Allow users to add tasks

--- src/index.js
+++ src/index.js
@@ -66,7 +66,27 @@ for (let i = 0; i < projectName.length; i++) {
 }
 
 taskBtn.addEventListener("click", (e) => {
+  let taskName2 = window.prompt("Whats your task!")
+  let taskDate2 = window.prompt("When is this task due?")
+  let taskPriority2 = window.prompt("How important is this task?")
 
+  let task2 = new TodoItem (taskName2, taskDate2, taskPriority2)
+
+  let divTask = document.createElement("div")
+  divTask.classList.add("task")
+  taskGroup.append(divTask)
+  
+  let divTaskName = document.createElement("div")
+  let divDueDate = document.createElement("div")
+  let divPriority = document.createElement("div")
+  
+  divTask.append(divTaskName)
+  divTask.append(divDueDate)
+  divTask.append(divPriority)
+  
+  divTaskName.textContent = task2.taskName
+  divDueDate.textContent =  task2.taskDate
+  divPriority.textContent =  task2.priority 
 })
 
 projectBtn.addEventListener("click", (e) => {
