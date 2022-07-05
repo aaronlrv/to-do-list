8b71bf3fa775c025b8d964bfdc7bca43f617f2de
aaronlrv <aaronvitharanage@gmail.com>
2022-07-05 11:21:50 +0400

    Modify add task function

--- src/index.js
+++ src/index.js
@@ -68,27 +68,30 @@ for (let i = 0; i < projectName.length; i++) {
 
 /// adds tasks 
 taskBtn.addEventListener("click", (e) => {
+  let projectsname2 = window.prompt("Whats your project name")
   let taskName2 = window.prompt("Whats your task!")
   let taskDate2 = window.prompt("When is this task due?")
   let taskPriority2 = window.prompt("How important is this task?")
 
   let task2 = new TodoItem (taskName2, taskDate2, taskPriority2)
 
-  let divTask = document.createElement("div")
-  divTask.classList.add("task")
-  taskGroup.append(divTask)
-  
-  let divTaskName = document.createElement("div")
-  let divDueDate = document.createElement("div")
-  let divPriority = document.createElement("div")
-  
-  divTask.append(divTaskName)
-  divTask.append(divDueDate)
-  divTask.append(divPriority)
-  
-  divTaskName.textContent = task2.taskName
-  divDueDate.textContent =  task2.taskDate
-  divPriority.textContent =  task2.priority 
+  if (projects[projectsName] === projectsname2) {
+    projects[projectsName].push(task2)
+  } 
+
+    if  (!(projectsname2 in projects)) {
+        projects[projectsname2] = []
+  }
+
+  projects[projectsname2].push(task2)
+
+
+  console.log(projects)
+
+
+  console.log(Object.keys(projects))
+
+
   console.log(projects)
 
 })
